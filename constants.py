2bc770e7497bda0aba72c55477dbf8145231097e
ArielLitran <ariellitran@gmail.com>
2026-01-02 16:34:51 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,20 +1,20 @@
 
-BSIZ = 3 # board side size
+BSIZ = 4 # board side size
 
 ST_PLAYER = 4 # stones per player
 
 # Define the colors we will use in RGB format
 BLACK =   (  0,   0,   0)
-GRAY =    (150, 150, 150) 
-WHITE =   (255, 255, 255)
+GRAY =    (255, 255, 255) 
+WHITE =   (45, 55, 70)
 # Chosen so that they are still friendly to colorblind people:
-BLUISH =  ( 26, 133, 255)
-REDDISH = (212,  17,  89)
+BLUISH =  (0, 255, 240)
+REDDISH = (255, 0, 127)
 PLAYER_COLOR = (BLUISH, REDDISH) 
 
 # Define the game window width and height and the slot size and separation in pixels
-SLOT = 100        # squares size
-SEP = 20          # squares separation
+SLOT = 125        # squares size
+SEP = 25          # squares separation
 ROOM = SLOT + SEP # extra room at sides 
 HEIGHT = BSIZ * SLOT + (BSIZ + 1) * SEP + ROOM # room for 3 squares with margin and internal separators and extra below
 WIDTH = HEIGHT + ROOM              # extra at both sides
