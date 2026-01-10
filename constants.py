ff0c81af06d66908e97a28fc957a66de09c3f3ac
ArielLitran <ariellitran@gmail.com>
2026-01-10 21:47:39 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,22 +1,23 @@
 
 # BSIZ (board side size) i ST_PLAYER (stones per player) s'ha passat al main per aixi puguin ser introduides, per tant deixen de ser constants
 
-# Define the colors we will use in RGB format
+# Definim els colors que farem servir en format RGB
 BLACK =   (  0,   0,   0)
 GRAY =    (255, 255, 255) 
 WHITE =   (45, 55, 70)
-# Chosen so that they are still friendly to colorblind people:
+# Escollits perquè siguin amigables per a les persones daltòniques:
 BLUISH =  (0, 255, 240)
 REDDISH = (255, 0, 127)
 PLAYER_COLOR = (BLUISH, REDDISH)
 
-# Define the game window width and height and the slot size and separation in pixels
-SLOT = 125        # squares size
-SEP = 25          # squares separation
-ROOM = SLOT + SEP # extra room at sides 
+# Defineix l'amplada i l'alçada de la finestra del joc i la mida i separació de les ranures en píxels
+SLOT = 125        # mida de les caselles
+SEP = 25          # separació de les caselles
+ROOM = SLOT + SEP # espai addicional als costats 
 
-#HEIGHT (room for 3 squares with margin and internal separators and extra below) i WIDTH (extra at both sides) tambe s'han passat al main
 
-RAD = SLOT / 3                     # circle radius
+#ALÇADA (espai per a 3 quadrats amb marge i separadors interns i extra a sota) i AMPLADA (extra a banda i banda) també s'han passat al main
+
+RAD = SLOT / 3                     # radi del cercle
 
 NO_PLAYER = -1
